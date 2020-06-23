739e60789f22949e98227e9d607d9824881acf11
Mo Nashawati <mo.nashawati@gmail.com>
2020-06-23 02:45:00 +0100

    Adding messages

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const games = require("./games.json");
 const app = express();
 
+const messages = [];
+
 app.get("/", (req, res) => {
   res.send("hello");
 });
@@ -18,6 +20,17 @@ if (port == null || port == "") {
 app.get("/port", (req, res) => {
   console.log(req.get("user-agent"));
   res.send(`This server runs on port: ${port}`);
+});
+
+app.get("/say", (req, res) => {
+  const msg = req.query.m;
+  messages.push(msg);
+  console.log(msg);
+  res.send(messages);
+});
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
 });
 
 app.listen(port, () => {
